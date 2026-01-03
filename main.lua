675bfb4ec374dae90d0daad667f290b7616e1c9f
Josip Bulić <Josip.Bulic@asee.io>
2026-01-03 19:58:21 +0100

    feature: Add minimal echo server

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
-print("Hello World!")
+local socket = require("socket")
+local server = assert(socket.bind("*", 0))
+local ip, port = server:getsockname()
+print("Please telnet to localhost on port " .. port)
+print("After connnecting, you have 10s to enter a line to be echoed")
+
+while true do
+    local client = server:accept()
+    client:settimeout(10)
+    local line, err = client:receive()
+
+    if not err then client:send(line .. "\n") end
+    client:close()
+end
