581c9f1d009969b51f0237a21bb86924cfc30bb4
Josip Bulić <Josip.Bulic@asee.io>
2026-01-04 12:07:32 +0100

    improvement: Read entire client request

--- main.lua
+++ main.lua
@@ -2,13 +2,23 @@ local socket = require("socket")
 local server = assert(socket.bind("*", 0))
 local ip, port = server:getsockname()
 print("Please telnet to localhost on port " .. port)
-print("After connnecting, you have 10s to enter a line to be echoed")
 
 while true do
     local client = server:accept()
     client:settimeout(10)
+    
+    local request_text = ""
     local line, err = client:receive()
 
-    if not err then client:send(line .. "\n") end
+    while not err do 
+        request_text = request_text .. line .. "\n"
+        if line == "" then break end
+        line, err = client:receive()
+    end    
+
+    print("Received request:\n" ..request_text)
+
+    local response = "HTTP/1.1 200 OK\r\n\r\nThis works!"
+    client:send(response)
     client:close()
 end
